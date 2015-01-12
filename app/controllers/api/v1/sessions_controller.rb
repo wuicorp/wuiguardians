2b447ba862da151091e7f7af611a6e68716fb216
Albert Viscasillas <aviscasillas@gmail.com>
2015-01-12 21:27:56 +0100

    Registration create is implicid in session create
    The flow tries ever to register a new user (phone number). If the user
    already exists, it will return the error forcing the app to the user
    authentication flow with confirmation_code.

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,39 @@
+module Api
+  module V1
+    class SessionsController < ApiController
+      def create
+        user = User.new(user_params)
+        app = Doorkeeper::Application.find_by(uid: params[:client_id],
+                                              secret: params[:client_secret])
+        if app && user.save
+          create_successful_response(app, user)
+        else
+          errors = app ? user.errors : { application: :invalid }
+          render json: errors, status: 422
+        end
+      end
+
+      private
+
+      def user_params
+        params.permit(:phone_prefix, :phone_number)
+      end
+
+      def find_or_create_access_token(app, user)
+        Doorkeeper::AccessToken.find_or_create_for(
+          app,
+          user.id,
+          nil,
+          Doorkeeper.configuration.access_token_expires_in,
+          Doorkeeper.configuration.refresh_token_enabled?
+        )
+      end
+
+      def create_successful_response(app, user)
+        access_token = find_or_create_access_token(app, user)
+        render(json: Doorkeeper::OAuth::TokenResponse.new(access_token).body,
+               status: 201)
+      end
+    end
+  end
+end
