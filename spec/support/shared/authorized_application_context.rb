e9f74f18c0d00f95746979ce26d97f6309cec107
Albert Viscasillas <aviscasillas@gmail.com>
2015-03-08 20:43:00 +0100

    Vehicles create action

---
+++ spec/support/shared/authorized_application_context.rb
@@ -0,0 +1,14 @@
+shared_context 'authorized application', authorized_application: true do
+  before do
+    request.headers[:authorization] = authorization
+    action.call
+  end
+
+  let(:developer) { create(:user, role: 'developer') }
+  let(:user) { create(:user) }
+  let(:application) { create(:application, owner: developer) }
+  let(:access_token) { create(:access_token, application: application) }
+  let(:token) { access_token.token }
+  let(:authorization) { "Bearer #{token}" }
+  let(:response_body) { JSON(response.body) }
+end
