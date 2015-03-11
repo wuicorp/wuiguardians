bcb7dad0aea4f271506eb2369efb308d55f4deb1
Albert Viscasillas <aviscasillas@gmail.com>
2015-03-11 21:42:43 +0100

    Fix db schema

--- db/migrate/20141030184852_create_vehicles.rb
+++ db/migrate/20141030184852_create_vehicles.rb
@@ -1,7 +1,6 @@
 class CreateVehicles < ActiveRecord::Migration
   def change
     create_table :vehicles do |t|
-      t.references :user, index: true
       t.string :identifier
       t.timestamps
     end
