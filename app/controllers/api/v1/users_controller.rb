09db3d45c40939223820fa850f8b807856dff2f1
Albert Viscasillas <aviscasillas@gmail.com>
2015-03-30 19:54:44 +0200

    Manage me as id in users endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,8 +1,21 @@
 module Api
   module V1
     class UsersController < ApiController
-      def me
-        responder.success(:get, current_owner)
+      def show
+        with_current_user do |user|
+          responder.success(:get, user)
+        end
+      end
+
+      private
+
+      def current_user
+        return @current_user ||=  current_owner if params[:id] == 'me'
+        nil
+      end
+
+      def with_current_user(&block)
+        current_user ? block.call(current_user) : responder.not_found
       end
     end
   end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -3,41 +3,50 @@ require 'rails_helper'
 describe Api::V1::UsersController do
   it { is_expected.to be_a_kind_of Api::V1::ApiController }
 
-  describe 'GET #me', authenticated_resource: true do
-    let(:action) { -> { get :me } }
-    let(:response_vehicles) { response_body['vehicles'] }
+  describe 'GET #show', authenticated_resource: true do
+    let(:action) { -> { get :show, request_params } }
 
-    it { is_expected.to respond_with(200) }
+    context 'with me id' do
+      let(:request_params) { { id: 'me' } }
+      let(:response_vehicles) { response_body['vehicles'] }
 
-    it 'responds with right parameters' do
-      expect(response_body).to include 'id'
-      expect(response_body).to include 'email'
-      expect(response_body).to include 'name'
-      expect(response_body).to_not include 'role'
-      expect(response_body).to_not include 'created_at'
-      expect(response_body).to_not include 'updated_at'
-    end
+      it { is_expected.to respond_with(200) }
 
-    context 'without vehicles' do
-      it 'responds with empty list of vehicles' do
-        expect(response_vehicles).to eq []
+      it 'responds with right parameters' do
+        expect(response_body).to include 'id'
+        expect(response_body).to include 'email'
+        expect(response_body).to include 'name'
+        expect(response_body).to_not include 'role'
+        expect(response_body).to_not include 'created_at'
+        expect(response_body).to_not include 'updated_at'
+      end
+
+      context 'without vehicles' do
+        it 'responds with empty list of vehicles' do
+          expect(response_vehicles).to eq []
+        end
+      end
+
+      context 'with vehicles' do
+        let(:vattrs1) { { id: 1, identifier: '1111111' } }
+        let(:vattrs2) { { id: 2, identifier: '2222222' } }
+
+        let(:before_context) do
+          build(:vehicle, vattrs1).tap { |v| v.users << current_owner }.save
+          build(:vehicle, vattrs2).tap { |v| v.users << current_owner }.save
+        end
+
+        it 'responds with owned vehicles' do
+          expect(response_vehicles.count).to eq 2
+          expect(response_vehicles).to include vattrs1.stringify_keys
+          expect(response_vehicles).to include vattrs2.stringify_keys
+        end
       end
     end
 
-    context 'with vehicles' do
-      let(:vattrs1) { { id: 1, identifier: '1111111' } }
-      let(:vattrs2) { { id: 2, identifier: '2222222' } }
-
-      let(:before_context) do
-        build(:vehicle, vattrs1).tap { |v| v.users << current_owner }.save
-        build(:vehicle, vattrs2).tap { |v| v.users << current_owner }.save
-      end
-
-      it 'responds with owned vehicles' do
-        expect(response_vehicles.count).to eq 2
-        expect(response_vehicles).to include vattrs1.stringify_keys
-        expect(response_vehicles).to include vattrs2.stringify_keys
-      end
+    context 'with any id different of me' do
+      let(:request_params) { { id: '1' } }
+      it { is_expected.to respond_with(404) }
     end
   end
 end
