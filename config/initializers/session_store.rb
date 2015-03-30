ab738efe80fcd53fd3aabd2bcefd1823108c8b32
Albert Viscasillas <aviscasillas@gmail.com>
2015-03-30 09:22:09 +0200

    Rename app

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_wuiweb_session'
+Rails.application.config.session_store :cookie_store, key: '_wuiguardians_session'
